dd9340ed6563b26cac37bece6f97e90bc99e9366
TetiankaSh <shh.tetiana@gmail.com>
2026-02-24 11:42:46 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
 import './App.scss';
+import './styles/globals.scss';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { HomePage } from './components/Home/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import { MobilePhones } from './components/MobilePhones/MobilePhones';
+import { PhonePage } from './components/PhonePage/PhonePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { Tablets } from './components/Tablets/Tablets';
+import { Accessories } from './components/Accessories/Accessories';
+import { TabletPage } from './components/TabletPage/TabletPage';
+import { AccessoryPage } from './components/AccessoryPage/AccessoryPage';
+import { Cart } from './components/Cart/Cart';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
+import { FavoritesProvider } from './context/FavoritesContext';
+import { Favorites } from './components/Favorites/Favorites';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => (
+  <div className="App">
+    <FavoritesProvider>
+      <ShoppingCartProvider>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/phones" element={<MobilePhones />} />
+            <Route path="/tablets" element={<Tablets />} />
+            <Route path="/accessories" element={<Accessories />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/phones/:itemId" element={<PhonePage />} />
+            <Route path="/tablets/:itemId" element={<TabletPage />} />
+            <Route path="/accessories/:itemId" element={<AccessoryPage />} />
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </ShoppingCartProvider>
+    </FavoritesProvider>
 
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+    <Footer />
+  </div>
+);
---
+++ src/components/TabletPage/TabletPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import tablets from '../../../public/api/tablets.json';
+import { ItemPage } from '../ItemPage/ItemPage';
+
+export const TabletPage: React.FC = () => {
+  return <ItemPage categoryName="Tablets" items={tablets} />;
+};
